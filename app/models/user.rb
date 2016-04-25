897c5a608c8094d137483c74e550097060561815
mike vallano <mikevallano@gmail.com>
2016-04-25 15:43:04 -0500

    configured devise confirmable and set default username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
+
+  before_save :default_username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+
+
+
+  def default_username
+    if self.username == ""
+      self.username = self.email[0..self.email.index("@")-1]
+    end
+  end
+
 end
