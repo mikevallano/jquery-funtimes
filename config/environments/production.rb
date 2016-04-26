590f86d6d7e7f7befe047d5f059a9d8cb344f316
mike vallano <mikevallano@gmail.com>
2016-04-26 10:46:42 -0500

    changed email config to raise errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://i-learned-that.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
