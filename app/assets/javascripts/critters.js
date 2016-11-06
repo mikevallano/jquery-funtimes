48d21440d246cab432cfd7107c9611dbca759824
mike vallano <mikevallano@gmail.com>
2016-11-06 10:27:32 -0600

    Reorganize JS File.

--- app/assets/javascripts/critters.js
+++ app/assets/javascripts/critters.js
@@ -6,50 +6,16 @@ $(document)
     });
   });
 
-  $(document).on('ajax:success', '#crit-form-modal form', function(e, crit) {
+  $(document).on('ajax:success', '#crit-form-modal form', function(e, crit) { //the second argument defines which element triggered the ajax:success. differentiates between form submission and delete or other ajax call.
       if (crit.errors) {
         toggleCritForm();
         $('#alert-msg').toggleClass('hidden').text("Error with the critter: " + crit.errors);
-          // $(this).find('.text-danger').show().html('<strong>Please fix the following errors: </strong><br/>' + res.errors.join('<br/>'))
       } else {
-        // function(last_crit) {
-          addCritToList(crit);
-          toggleCritForm();
-          // this clears out the form fields after successful submission
-          $("#crit-form-modal form")[0].reset();
-      // }
+        addCritToList(crit);
+        toggleCritForm();
+        $("#crit-form-modal form")[0].reset(); // this clears out the form fields after successful submission
       }
   });
-
-
-  // $("#crit-form-modal form").submit(function(e) {
-  //   // both of these lines are needed to prevent double form submitting
-  //   e.preventDefault();
-  //   e.stopImmediatePropagation();
-
-  //   var new_critter = {
-  //     name: $('#critter_name').val(),
-  //     description: $('#critter_description').val(),
-  //   };
-
-    // $.ajax({
-    //   type: 'POST',
-    //   url: '/critters.json',
-    //   data: JSON.stringify(new_critter),
-    //   // the following two lines were needed to prevent Rails errors
-    //   contentType: "application/json; charset=utf-8",
-    //   dataType: "json",
-    //   success: function(last_crit) {
-    //     addCritToList(last_crit);
-    //     toggleCritForm();
-    //     // this clears out the form fields after successful submission
-    //     $("#crit-form-modal form")[0].reset();
-    //     // $('#critter_name').val('');
-    //     // $('#critter_description').val('');
-    //   } //success
-    // }); //ajax
-
-  // }); //end form submit function
 
   function addCritToList(critter) {
     $('#crit-table-body').append(
@@ -63,42 +29,71 @@ $(document)
   } //end addCritToList
 
   $('.new-crit-btn').click(function() {
-    toggleCritForm();
+    toggleCritForm(); //show/hide form
   })
 
-  function toggleCritForm() {
-    $('#crit-form-modal').modal('toggle');
-  }
+  $(document).on('click', '.edit-crit-link', function() { // syntax for adding event listeners on items added to the dom
+    var tr = $(this).closest('tr');
+    var critId = tr.prop("id");
+    $.get('/critters/'+critId+'.json', function(critter) { // GET request to get entire critter object
+      editCritForm(critter, tr);
+    });
+  });
 
   function editCritForm(critter, tr) {
     toggleCritForm();
     $('#crit-form-modal form #critter_name').val(critter.name);
     $('#crit-form-modal form #critter_description').val(critter.description);
-    $('#crit-form-modal form').attr('action', critter.edit_url);
-    $('#crit-form-modal form').attr('method', 'patch');
-    $('#crit-form-modal form input').first().attr('method', 'put');
-    $('#crit-form-modal form input[type=submit]').attr('value', 'Update this Critter');
+    $('#crit-form-modal form').attr('action', critter.edit_url); // send to #edit instead of #create action
+    $('#crit-form-modal form').attr('method', 'patch'); // change method to patch
+    $('#crit-form-modal form input').first().attr('method', 'put'); // change method to put
+    $('#crit-form-modal form input[type=submit]').attr('value', 'Update this Critter'); // change to "update" instead of 'create'
     $("#crit-form-modal form").submit(function(e) {
-      tr.hide();
+      tr.hide(); // after successful ajax call, the object will be appended to the table. hiding the former tr eliminates duplicates
     })
   }
-
-// syntax for adding event listeners on items added to the dom
-  $(document).on('click', '.edit-crit-link', function() {
-    var tr = $(this).closest('tr');
-    var critId = tr.prop("id");
-    $.get('/critters/'+critId+'.json', function(critter) {
-      editCritForm(critter, tr);
-    });
-  });
 
   $(document).on('click', '.delete-crit-link', function() {
     var tr = $(this).closest('tr');
     tr.hide();
   });
 
+  function toggleCritForm() {
+    $('#crit-form-modal').modal('toggle');
+  }
+
 }); // end document ready
 
+
+// AJAX code when not using built-in rails functionality
+// $("#crit-form-modal form").submit(function(e) {
+//   // both of these lines are needed to prevent double form submitting
+//   e.preventDefault();
+//   e.stopImmediatePropagation();
+
+//   var new_critter = {
+//     name: $('#critter_name').val(),
+//     description: $('#critter_description').val(),
+//   };
+
+  // $.ajax({
+  //   type: 'POST',
+  //   url: '/critters.json',
+  //   data: JSON.stringify(new_critter),
+  //   // the following two lines were needed to prevent Rails errors
+  //   contentType: "application/json; charset=utf-8",
+  //   dataType: "json",
+  //   success: function(last_crit) {
+  //     addCritToList(last_crit);
+  //     toggleCritForm();
+  //     // this clears out the form fields after successful submission
+  //     $("#crit-form-modal form")[0].reset();
+  //     // $('#critter_name').val('');
+  //     // $('#critter_description').val('');
+  //   } //success
+  // }); //ajax
+
+// }); //end form submit function
 
   // Alternate Syntax for ajax request:
   // $.ajax({
--- app/controllers/critters_controller.rb
+++ app/controllers/critters_controller.rb
@@ -4,7 +4,6 @@ class CrittersController < ApplicationController
   # GET /critters
   # GET /critters.json
   def index
-    sleep 1
     @critters = Critter.all
   end
 
