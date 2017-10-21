e120dde5b9c6375843a57471dcbfe91540b8333b
mike vallano <mikevallano@gmail.com>
2017-10-21 11:16:18 -0500

    Session and cookie testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :check_session_and_cookies
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def check_session_and_cookies
+    cookies.encrypted[:tester] = { value: 'taco' }
+    cookies[:taco] = { value: 'sauce' }
+    puts "session: #{session.values}"
+    puts "cookies: #{cookies}"
   end
 end
