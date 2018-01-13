6604453e9f2399e693b1c1e68e08a38903ef151d
mike vallano <mikevallano@gmail.com>
2018-01-13 09:29:54 -0600

    Tweak styles on articles

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,10 +1,9 @@
-$(document)
-  .on('ajax:success', '.edit_article', function(e, res) {
-      console.log('res: ',res)
-      $('#article-name').text(res.updated_name)
-      $('#article-description').text(res.updated_description)
-      $('#article-form-holder').addClass('hidden')
-    })
+$(document).on('ajax:success', '.edit_article', function(e, res) {
+  console.log('res: ',res)
+  $('#article-name').text(res.updated_name)
+  $('#article-description').text(res.updated_description)
+  $('#article-form-holder').addClass('hidden')
+})
 
 $(function(){
   $('#article-edit-btn').click(function(){
